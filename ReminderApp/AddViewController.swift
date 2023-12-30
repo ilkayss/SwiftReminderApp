e6f2852fa60acfb0e890a34512a4d0fdc03c404f
ilkay <ilkayahmet585@gmail.com>
2023-12-30 17:36:22 +0300

    Initial Commit

---
+++ ReminderApp/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddViewController.swift
+//  ReminderApp
+//
+//  Created by ahmet on 2.01.2024.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
